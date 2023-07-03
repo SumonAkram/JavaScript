a9cd30898b6bea7473ab2a3e2d92db953a089c99
Md Shamsul Akram <40783457+SumonAkram@users.noreply.github.com>
2023-07-04 00:22:22 +0200

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,16 @@
+// Declare a variable
+let greeting = "Hello, World!";
+
+// Print the greeting to the console
+console.log(greeting);
+
+// Create a function
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Call the function and store the result in a variable
+let sum = addNumbers(5, 10);
+
+// Print the result to the console
+console.log("The sum is: " + sum);
